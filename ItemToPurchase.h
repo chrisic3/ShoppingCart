3c74f9db2d21ff2e38834ddc4ee804887116e71c
chrisic3 <chris_ic3@hotmail.com>
2020-03-10 11:46:59 -0500

    Update ItemToPurchase.h

--- ItemToPurchase.h
+++ ItemToPurchase.h
@@ -2,7 +2,19 @@
 #define ITEMTOPURCHASE_H
 
 class ItemToInput {
+   public: 
+       ItemToInput();
+       void SetName(string name);
+       string GetName() const;
+       void SetPrice(int price);
+       int GetPrice() const;
+       void SetQuantity(int quantity);
+       int GetQuantity() const;
   
+   private:
+       string itemName;
+       int itemPrice;
+       int itemQuantity;
 };
 
 #endif
